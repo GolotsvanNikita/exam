99ba8df3f271753bda99ad58182651a8223dd196
Nikita Holotsvan <hohoko555888@gmail.com>
2025-07-17 01:20:02 +0300

    small change

--- exam/src/components/Navigation.jsx
+++ exam/src/components/Navigation.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Navigation.css';
+import {Link} from "react-router-dom";
 
 export function Navigation()
 {
     return(
         <nav className='navContainer'>
-            <a href=""><img src="../public/logo.png" alt="logo" className='logo'/></a>
+            <Link to='/'><img src="../public/logo.png" alt="logo" className='logo'/></Link>
             <div className='inputNav'>
                 <div className='combine'>
                     <input type="text" placeholder='Search'/>
@@ -14,9 +15,9 @@ export function Navigation()
                 <button className='mic'><img src="../public/mic.png" alt="mic"/></button>
             </div>
             <div className='buttons'>
-                <a href="" className='kvadrat'><img src="../public/kvadrat.svg" alt="kvadrat"/></a>
-                <a href="" className='bell'><img src="../public/Bell.svg" alt="ring"/></a>
-                <a href="" className='ava'><img src="../public/ava.png" alt="ava"/></a>
+                <a href="#" className='kvadrat'><img src="../public/kvadrat.svg" alt="kvadrat"/></a>
+                <a href="#" className='bell'><img src="../public/Bell.svg" alt="ring"/></a>
+                <a href="#" className='ava'><img src="../public/ava.png" alt="ava"/></a>
             </div>
         </nav>
     );
